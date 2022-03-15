9f94001f1640e9017a45ebebaefb9ff6e869adaf
Christian Piccinini <christianpiccinini4@gmail.com>
2022-03-15 09:25:40 -0300

    deploy config

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 async function startDB() {
-    await mongoose.connect('mongodb://localhost/citas-filosoficas',{
+    await mongoose.connect(process.env.DB_LOCATION,{
         useNewUrlParser: true
     })
     console.log('DB connected');
